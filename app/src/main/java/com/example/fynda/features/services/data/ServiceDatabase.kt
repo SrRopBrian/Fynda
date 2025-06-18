27826151d29be758be18055bba71741c92a6e76a
SrRopBrian <kipngenobrian070@gmail.com>
2025-06-18 12:48:03 +0300

    Added the databaseBuilder

--- app/src/main/java/com/example/fynda/features/services/data/ServiceDatabase.kt
+++ app/src/main/java/com/example/fynda/features/services/data/ServiceDatabase.kt
@@ -1,4 +1,31 @@
 package com.example.fynda.features.services.data
 
-class ServiceDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [Service::class], version = 1)
+abstract class ServiceDatabase : RoomDatabase() {
+    abstract fun dao(): ServiceDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ServiceDatabase? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getServiceDatabase(context: Context): ServiceDatabase? {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ServiceDatabase::class.java,
+                    "service_database"
+                ).build()
+                INSTANCE = instance
+                INSTANCE
+            }
+        }
+    }
 }
